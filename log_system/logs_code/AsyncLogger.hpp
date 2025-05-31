89fa08a61deac5215a3388086335274ffe961f30
linchao-abc <2748558379@qq.com>
2025-06-01 00:13:17 +0800

    创建文件

--- log_system/logs_code/AsyncLogger.hpp
+++ log_system/logs_code/AsyncLogger.hpp
@@ -1,3 +1,15 @@
 #pragma once
 // 异步日志器类（Asynchronous Logger），它在日志系统中用于异步地将日志写入
 // 文件或终端，避免日志写入操作阻塞主线程，从而提高程序性能和响应速度
+#include "ThreadPoll.hpp"
+
+extern ThreadPool *tp;
+
+namespace mylog {
+    class AsyncLogger {
+    public:
+        using ptr = std::shared_ptr<AsyncLogger>;
+        
+        
+    };
+}
