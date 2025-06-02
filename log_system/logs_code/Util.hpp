b42e91eaee57e4f658c4e4885496a2b5fa7fe887
linchao-abc <2748558379@qq.com>
2025-06-02 23:49:42 +0800

    修改头文件

--- log_system/logs_code/Util.hpp
+++ log_system/logs_code/Util.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sys/types.h>
 #include <json/json.h>
+#include <ctime>
 using std::cout;
 using std::endl;
 
@@ -142,32 +143,32 @@ namespace mylog {
                 return json_data;
             }
 
-        private:
-            JsonData() {
-                std::string content;
-                mylog::Util::File file;
-                if(file.GetContent(&content, "../../log_system/logs_code/config.conf") == false) {
-                    cout << __FILE__ << __LINE__ << "open config.conf failed" << endl;
-                    perror(NULL);
+            private:
+                JsonData() {
+                    std::string content;
+                    mylog::Util::File file;
+                    if(file.GetContent(&content, "../../log_system/logs_code/config.conf") == false) {
+                        cout << __FILE__ << __LINE__ << "open config.conf failed" << endl;
+                        perror(NULL);
+                    }
+                    Json::Value root;
+                    mylog::Util::JsonUtil::UnSerialize(content, &root); // 反序列化，把内容转成json value格式
+                    buffer_size = root["buffer_size"].asInt64();
+                    threshold = root["threshold"].asInt64();
+                    linear_growth = root["linear_growth"].asInt64();
+                    flush_log = root["flush_log"].asInt64();
+                    backup_addr = root["backup_addr"].asString();
+                    backup_port = root["backup_port"].asInt();
+                    thread_count = root["thread_count"].asInt();
                 }
-                Json::Value root;
-                mylog::Util::JsonUtil::UnSerialize(content, &root); // 反序列化，把内容转成json value格式
-                buffer_size = root["buffer_size"].asInt64();
-                threshold = root["threshold"].asInt64();
-                linear_growth = root["linear_growth"].asInt64();
-                flush_log = root["flush_log"].asInt64();
-                backup_addr = root["backup_addr"].asString();
-                backup_port = root["backup_port"].asInt();
-                thread_count = root["thread_count"].asInt();
-            }
-        public:
-            size_t buffer_size; // 缓冲区基础容量
-            size_t threshold; // 倍数扩容阈值
-            size_t linear_growth; // 线性增长容量
-            size_t flush_log; // 控制日志同步到磁盘的时机，默认是0，1调用fflush，2调用fsync
-            std::string backup_addr;
-            uint16_t backup_port;
-            size_t thread_count;
+            public:
+                size_t buffer_size; // 缓冲区基础容量
+                size_t threshold; // 倍数扩容阈值
+                size_t linear_growth; // 线性增长容量
+                size_t flush_log; // 控制日志同步到磁盘的时机，默认是0，1调用fflush，2调用fsync
+                std::string backup_addr;
+                uint16_t backup_port;
+                size_t thread_count;
         };
     }
 }
