e311be276dea65b301b50c933db34eb88821462e
linchao-abc <2748558379@qq.com>
2025-06-06 00:22:55 +0800

    添加异步日志器类

--- log_system/logs_code/AsyncLogger.hpp
+++ log_system/logs_code/AsyncLogger.hpp
@@ -1,6 +1,17 @@
 #pragma once
 // 异步日志器类（Asynchronous Logger），它在日志系统中用于异步地将日志写入
 // 文件或终端，避免日志写入操作阻塞主线程，从而提高程序性能和响应速度
+#include <atomic>
+#include <mutex>
+#include <cassert>
+#include <cstdarg>
+#include <memory>
+#include <unistd.h>
+#include "Level.hpp"
+#include "AsyncWorker.hpp"
+#include "Message.hpp"
+#include "LogFlush.hpp"
+#include "backlog/CliBackupLog.hpp"
 #include "ThreadPoll.hpp"
 
 extern ThreadPool *tp;
@@ -9,7 +20,176 @@ namespace mylog {
     class AsyncLogger {
     public:
         using ptr = std::shared_ptr<AsyncLogger>;
+        AsyncLogger(const std::string &logger_name, std::vector<LogFlush::ptr> &flushs, AsyncType type)
+                : logger_name_(logger_name), // 初始化日志器名字
+                  flushs_(flushs.begin(), flushs.end()), // 添加实例化方式给日志器，如日志输出到文件还是标准输出，可能有多种
+                  asyncworker(std::make_shared<AsyncWorker>(
+                    std::bind(&AsyncLogger::RealFlush, this, std::placeholders::_1),
+                    type)) {}
+
+        virtual ~AsyncLogger() {};
+        std::string Name() {
+            return logger_name_;
+        }
+
+        // 该函数是特定日志级别的日志信息的格式化，当外部调用该日志器时，使用debug模式的日志就会进来
+        // 在serialize时把日志信息中的日志级别定义为DEBUG
+        void Debug(const std::string &file, size_t line, const std::string format, ...) {
+            // 获取可变参数列表中的格式
+            va_list va;
+            va_start(va, format);
+            char* ret;
+            int r = vasprintf(&ret, format.c_str(), va);
+            if(r == -1) {
+                perror("vasprintf failed!!!:");
+            }
+            va_end(va); // 将va指针悬空
+            // 生成格式化日志信息并写文件
+            serialize(LogLevel::value::DEBUG, file, line, ret);
+            free(ret);
+            ret = nullptr;
+        }
+
+        void Info(const std::string &file, size_t line, const std::string format, ...) {
+            // 获取可变参数列表中的格式
+            va_list va;
+            va_start(va, format);
+            char* ret;
+            int r = vasprintf(&ret, format.c_str(), va);
+            if(r == -1) {
+                perror("vasprintf failed!!!:");
+            }
+            va_end(va); // 将va指针悬空
+            // 生成格式化日志信息并写文件
+            serialize(LogLevel::value::INFO, file, line, ret);
+            free(ret);
+            ret = nullptr;
+        }
+
+        void Warn(const std::string &file, size_t line, const std::string format, ...) {
+            // 获取可变参数列表中的格式
+            va_list va;
+            va_start(va, format);
+            char* ret;
+            int r = vasprintf(&ret, format.c_str(), va);
+            if(r == -1) {
+                perror("vasprintf failed!!!:");
+            }
+            va_end(va); // 将va指针悬空
+            // 生成格式化日志信息并写文件
+            serialize(LogLevel::value::WARN, file, line, ret);
+            free(ret);
+            ret = nullptr;
+        }
+
+        void Error(const std::string &file, size_t line, const std::string format, ...) {
+            // 获取可变参数列表中的格式
+            va_list va;
+            va_start(va, format);
+            char* ret;
+            int r = vasprintf(&ret, format.c_str(), va);
+            if(r == -1) {
+                perror("vasprintf failed!!!:");
+            }
+            va_end(va); // 将va指针悬空
+            // 生成格式化日志信息并写文件
+            serialize(LogLevel::value::ERROR, file, line, ret);
+            free(ret);
+            ret = nullptr;
+        }
+
+        void Fatal(const std::string &file, size_t line, const std::string format, ...) {
+            // 获取可变参数列表中的格式
+            va_list va;
+            va_start(va, format);
+            char* ret;
+            int r = vasprintf(&ret, format.c_str(), va);
+            if(r == -1) {
+                perror("vasprintf failed!!!:");
+            }
+            va_end(va); // 将va指针悬空
+            // 生成格式化日志信息并写文件
+            serialize(LogLevel::value::FATAL, file, line, ret);
+            free(ret);
+            ret = nullptr;
+        }
+
+
+
         
-        
+    protected:
+        // 在这里将日志消息组织起来，并写入文件
+        void serialize(LogLevel::value level, const std::string &file, size_t line, char* ret) {
+            LogMessage msg(level, file, line, logger_name_, ret);
+            std::string data =  msg.format();
+            if(level == LogLevel::value::FATAL || level == LogLevel::value::ERROR) {
+                try {
+                    auto ret = tp->enqueue(start_backup, data);
+                    ret.get();
+                }
+                catch (const std::runtime_error &e) {
+                    // 该线程池没有把stop设置为true，所以不做处理
+                    std::cout << __FILE__ << __LINE__ << "thread pool closed" << std::endl;
+                }
+            }
+            // 获取到string类型的日志信息后就可以输出到异步缓冲区了，异步工作其后续会将其刷入磁盘
+            Flush(data.c_str(), data.size());
+        }
+
+        void Flush(const char* data, size_t len) {
+            asyncworker->Push(data, len); // Push函数本身是线程安全的，这里不加锁
+        }
+
+        void RealFlush(Buffer& buffer) {
+            // 由异步线程进行实际写文件
+            if(flushs_.empty()) {
+                return;
+            }
+            for(auto &e : flushs_) {
+                // e是Flush这个类，即控制把日志输出到那个类
+                e->Flush(buffer.Begin(), buffer.ReadableSize());
+            }
+        }
+
+
+    protected:
+        std::mutex mtx_;
+        std::string logger_name_;
+        std::vector<LogFlush::ptr> flushs_; // 输出到指定方向
+        // std::vector<LogFlush> flush_;不能使用logflush作为元素类型，logflush是纯虚羸，不能实例化
+        mylog::AsyncWorker::ptr asyncworker; 
+    };
+
+    // 日治器建造
+    class LoggerBuilder {
+        public:
+            using ptr = std::shared_ptr<LoggerBuilder>;
+            void BuildLoggerName(const std::string &name) {
+                logger_name_ = name;
+            }
+            void BuildLoggerType(AsyncType type) {
+                async_type_ = type;
+            }
+            template <typename FlushType, typename... Args>
+            void BuildLoggerFlush(Args &&...args) {
+                flushs_.emplace_back(
+                    LogFlushFactory::CreateLog<FlushType>(std::forward<Args>(args)...)
+                );
+            }
+
+            AsyncLogger::ptr Build() {
+                assert(logger_name_.empty() == false); // 必须有日治器名称
+                // 如果写日志方式没有指定， 那么采用默认的标准输出
+                if(flushs_.empty()) {
+                    flushs_.emplace_back(std::make_shared<StdoutFlush>());
+                }
+                return std::make_shared<AsyncLogger>(
+                    logger_name_, flushs_, async_type_);
+            }
+
+        protected:
+            std::string logger_name_ = "async_logger"; // 日治器名称
+            std::vector<mylog::LogFlush::ptr> flushs_; // 写日志方式
+            AsyncType async_type_ = AsyncType::ASYNC_SAFE; // 用于控制缓冲区是否增长
     };
 }
